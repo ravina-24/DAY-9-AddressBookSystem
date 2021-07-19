658b64919a3452693af142ba8aeea10312eddace
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-19 22:46:43 +0530

    [Ravina] Add: Declaring the details of a person in the address book

---
+++ UC_1_Details.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_9_AddressBookSystem
+{
+   public  class Details
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
